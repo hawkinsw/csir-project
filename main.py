befecee2a076a5cf38dff76c18ce066bcedf0a4d
Will Hawkins <hawkinsw@opentechinstitute.org>
2014-11-11 19:23:51 -0500

    Generate appropriate runner based on source code type.

--- main.py
+++ main.py
@@ -6,7 +6,41 @@ from doxygen import doxygen
 import urllib2
 from github import github
 from github3 import models
+from javadoc import javadoc
+from git import Repo
+import tempfile
+import shutil
 #import logging
+
+def get_runner(language,
+	repo_source_name,
+	repo_source_package,
+	repo_source_url,
+	repo_dir):
+	if language == "Java":
+		return javadoc.JavaDocRunner(repo_source_name, repo_source_package, repo_source_url, repo_dir)
+	elif language == "C++":
+		return doxygen.DoxygenRunner(repo_source_name, repo_source_package, repo_source_url, repo_dir)
+	else:
+		return None
+
+def analyze(repo, repo_language):
+	repo_dir = tempfile.mkdtemp()
+	repo_source_name = str(repo)
+	repo_source_package = repo.name
+	repo_source_url = repo.git_url
+
+	print("repo: " + repo_source_name)
+	print("repo package: " + repo_source_package)
+	print("repo url: " + repo_source_url)
+
+	remote = Repo.clone_from(repo_source_url, repo_dir)
+	runner = get_runner(repo_language, repo_source_name, repo_source_package, repo_source_url, repo_dir)
+	if runner == None:
+		return
+	runner.run("localhost", "ir", "ir", "ir")
+	print(repo_dir)
+	shutil.rmtree(repo_dir)
 
 def main():
 	pd = doxygen.PotentialDoxygenSite("http://www.abisource.com/doxygen/")
--- run.py
+++ run.py
@@ -2,10 +2,16 @@
 
 from __future__ import print_function
 from javadoc import javadoc
+from doxygen import doxygen
 
-def test():
+def testJavaDoc():
 	jd = javadoc.JavaDocRunner("Testing1", "Testing1.gz", "URL", "/home/hawkinsw/code/csir/project/javadoc/src/")
 	jd.run("localhost", "ir", "ir", "ir")
 
+def testDoxygen():
+	dx = doxygen.DoxygenRunner("Testing1", "Testing1.gz", "URL", "/tmp/gnuradio/gnuradio-3.7.5.1")
+	dx.run("localhost", "ir", "ir", "ir")
+
+
 if __name__ == "__main__":
-	test()
+	testDoxygen()
