45a08b4daedef7e252c15e2429b9c4927423c472
Will Hawkins <hawkinsw@opentechinstitute.org>
2015-01-02 22:54:20 -0500

    Bullet-proof Java flow code.

--- flow/flow.py
+++ flow/flow.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python
 
 import docdb
+import re
+
+# From http://stackoverflow.com/questions/1175208/elegant-python-function-to-convert-camelcase-to-camel-case
+first_cap_re = re.compile('(.)([A-Z][a-z]+)')
+all_cap_re = re.compile('([a-z0-9])([A-Z])')
+no_numbers_re = re.compile('[0-9]')
+def decamel(variable):
+	v = first_cap_re.sub(r'\1_\2', variable)
+	v = all_cap_re.sub(r'\1_\2', v)
+	return no_numbers_re.sub(r'', v)
+
+def splits(variable):
+	# strip beginning and ending _s
+	variable_ = variable.strip("_")
+	variable_ = decamel(variable_)
+	return variable_.split("_")
 
 class Flow(object):
 	def __init__(self, package_id, db):
@@ -8,6 +24,6 @@ class Flow(object):
 		self.db = db
 	def calculate_flow(self):
 		for source_id in self.db.sourceForPackage(self.package_id):
-			print(source_id)
+			#print(source_id)
 			(source, method_name) = self.db.sourceForSource(source_id)
 			self.visit(source_id, method_name, source)
--- javadoc/javaflow.py
+++ javadoc/javaflow.py
@@ -6,22 +6,49 @@ import plyj.parser
 import plyj.model
 from flow import flow
 import docdb
-
 def collect_variables(statement):
 	left = []
 	right = []
 	base = []
-	print(str(statement))
+	target = []
+	arguments = []
+
+	# In case the statement is a binary operation
+	# v = lhs + rhs
+	# v = lhs - rhs
+	# etc
+	# descend down the lhs and rhs:
 	if hasattr(statement, 'lhs'):
 		left = collect_variables(statement.lhs)
 	if hasattr(statement, 'rhs'):
 		right = collect_variables(statement.rhs)
-	if hasattr(statement, 'name'):
+
+	# Now, take care of different specific types
+	# of statements.
+	if type(statement) is plyj.model.Type:
 		base = [str(statement.name)]
+
+	elif type(statement) is plyj.model.InstanceCreation:
+		base = collect_variables(statement.type)
+		for ta in statement.type_arguments:
+			arguments.extend(collect_variables(ta))
+		for arg in statement.arguments:
+			arguments.extend(collect_variables(arg))
+
+	elif type(statement) is plyj.model.MethodInvocation:
+		base = [str(statement.name)]
+		target = collect_variables(statement.target)
+		for arg in statement.arguments:
+			arguments.extend(collect_variables(arg))
+
 	elif type(statement) is plyj.model.Name:
 		base = [str(statement.value)]
+
+	# Combine what we have.
 	base.extend(left)
 	base.extend(right)
+	base.extend(target)
+	base.extend(arguments)
 	return base
 
 class JavaFlow(flow.Flow):
@@ -32,18 +59,36 @@ class JavaFlow(flow.Flow):
 		code = ("class A { void t() { %s }}" % (code))
 		parser = plyj.parser.Parser()
 		tree = parser.parse_string(code)
-		relateds = {}
+		package_id = self.package_id
 		class JavaVisitor(plyj.model.Visitor):
 			def visit_MethodDeclaration(self, method_decl):
 				if method_decl.body is not None:
 					for statement in method_decl.body:
 						if type(statement) is plyj.model.Assignment:
-							relateds[str(statement.lhs.value)] = collect_variables(statement.rhs)
-		tree.accept(JavaVisitor())
+							print(statement)
+							word1 = None
+							if type(statement.lhs) is plyj.model.Name:
+								word1 = str(statement.lhs.value)
+							elif hasattr(statement.lhs, 'name'):
+								word1 = str(statement.lhs.name)
+							words2 = []
+							for v in collect_variables(statement.rhs):
+								words2.extend(flow.splits(v))
+							if word1 is not None and len(words2) != 0:
+								words1 = flow.splits(word1)
+								for word1 in words1:
+									for word2 in words2:
+										if word1.lower() != word2.lower():
+											print(word1.lower() + " ?=> " + word2.lower())
+											#db.addWordRelationship(package_id,
+												#word1.lower(),
+												#word2.lower())
+		if tree != None:
+			tree.accept(JavaVisitor())
 
 if __name__ == "__main__":
 	db = docdb.DocDb("localhost", "ir", "ir", "ir")
 	db.connect()
-	jf = JavaFlow(1, db)
+	jf = JavaFlow(66, db)
 	jf.calculate_flow()
 	db.disconnect()
