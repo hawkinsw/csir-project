7d0c5dbf67a9d723099330d15633a62368d70af9
Will Hawkins <hawkinsw@opentechinstitute.org>
2015-01-02 22:49:17 -0500

    Update SQL to track parents

--- sql/clean.sql
+++ sql/clean.sql
@@ -5,3 +5,5 @@ delete from package;
 delete from source;
 delete from dependency;
 delete from parameter;
+delete from synonyms;
+delete from parents;
--- sql/create.sql
+++ sql/create.sql
@@ -5,6 +5,8 @@ drop table if exists source;
 drop table if exists documentation;
 drop table if exists parameter;
 drop table if exists dependency;
+drop table if exists synonyms;
+drop table if exists parents;
 
 create table package (
 	id integer not null primary key auto_increment,
@@ -21,6 +23,13 @@ create table source (
 	return_type varchar(255),
 	parameter_count integer not null default 1,
 	source text);
+
+create table parents (
+	package_id integer not null,
+	source_id integer not null,
+	parent_id integer,
+	parent_name varchar(255),
+	primary key (package_id, source_id, parent_id, parent_name));
 
 create table parameter (
 	id integer not null primary key auto_increment,
@@ -41,3 +50,10 @@ create table dependency (
 	source_id integer not null,
 	depends_on_id integer,
 	depends_on_name varchar(255));
+
+create table synonyms (
+	id integer not null primary key auto_increment,
+	package_id integer not null,
+	count integer not null default 0,
+	word1 varchar(255),
+	word2 varchar(255));
